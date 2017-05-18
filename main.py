5e900fde044d3c903e645c79387fbc21c183f32c
lpfjustino <lpfjustino@gmail.com>
2017-05-18 17:28:00 -0300

    Preliminary preprocessing finished with GNB fit.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from math import isnan
+
+from sklearn.naive_bayes import GaussianNB
 
 from enum import Enum
 
@@ -32,12 +33,37 @@ def preprocess(data_frame):
         avg = int(avg_ages[p.Sex][p.Pclass])
         data_set.set_value(i,'Age', avg)
 
+    # # Replace empty fares by the average fare
+    # fares = data_set.loc[:, 'Fare']
+    # avg = fares.mean()
+    # missing_fares = data_set.loc[ages.isnull() == True, :]
+    # print(fares)
+    # print(missing_fares, avg)
+    # for i, p in missing_fares.iterrows():
+    #     data_set.set_value(i,'Fare', avg)
+
     # Discretization of ages
     bins = [2, 10, 18, 60]
-    disc_ages = pd.Series(np.digitize(ages, bins)).map(lambda x: Age(x).name)
+    #disc_ages = pd.Series(np.digitize(ages, bins)).map(lambda x: Age(x).name)
+    disc_ages = pd.Series(np.digitize(ages, bins))
     data_set.loc[:, 'Age'] = disc_ages
 
-    print(data_set)
+    # Discretization of fares
+    fares = data_set.loc[:, 'Fare']
+    bins = np.linspace(0, 150, 10)
+    disc_fares = pd.Series(np.digitize(fares, bins))
+    data_set.loc[:, 'Fare'] = disc_fares
+
+    # Replacing gender
+    data_set.replace(['male','female'], [0,1], inplace=True)
+
+    # Replacing embarkation port and filling unknown ones with most frequent embark port
+    data_set.replace(['C', 'Q', 'S'], [0,1,2], inplace=True)
+    most_freq_port = data_set.loc[:,'Embarked'].max()
+    is_missing = data_set.isnull().values.sum(axis=1)
+    missing_emb = data_set.loc[is_missing == 1, :]
+    for i, miss in missing_emb.iterrows():
+        data_set.set_value(i,'Embarked', most_freq_port)
 
     return id, data_set, expected
 
@@ -45,4 +71,8 @@ def preprocess(data_frame):
 # Reading titanic dataset
 data_set = pd.read_csv("train.csv")
 
-preprocess(data_set)
+
+id, data_set, expected = preprocess(data_set)
+
+gnb = GaussianNB()
+gnb.fit(data_set, expected)
